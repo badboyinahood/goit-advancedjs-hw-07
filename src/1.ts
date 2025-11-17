659d41263f031754cabc22a5fd6de74ecd15e06a
Konstantin <65172983+badboyinahood@users.noreply.github.com>
2025-11-17 06:59:50 +0200

    123

--- src/1.ts
+++ src/1.ts
@@ -5,15 +5,13 @@
 */
 
 class Student {
-  public name: string;
-  public age: number;
-  public grade: string;
-
-  constructor(name: string, age: number, grade: string) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
-  }
+  constructor(
+    public name: string,
+    public age: number,
+    public grade: string
+  ) {}
 }
 
-export {};
+// TEST
+const std = new Student("Dmytro", 20, "A");
+console.log(std);
---
+++ test.bat
@@ -0,0 +1,18 @@
+@echo off
+cls
+
+echo =========== TASK 1 ============
+node dist/1.js
+
+echo.
+echo =========== TASK 2 ============
+node dist/2.js
+
+echo.
+echo =========== TASK 3 ============
+node dist/3.js
+
+echo.
+echo =========== TASK 4 ============
+node dist/4.js
+pause
